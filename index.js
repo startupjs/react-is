f45d25c792cc3d010ed4206659351cf1f14ea3cf
Pavel Zhukov <cray0000@gmail.com>
2020-06-13 18:48:14 +0300

    Add default export

--- index.js
+++ index.js
@@ -175,3 +175,34 @@ export {
   isValidElementType,
   typeOf
 }
+
+export default {
+  AsyncMode,
+  ConcurrentMode,
+  ContextConsumer,
+  ContextProvider,
+  Element,
+  ForwardRef,
+  Fragment,
+  Lazy,
+  Memo,
+  Portal,
+  Profiler,
+  StrictMode,
+  Suspense,
+  isAsyncMode,
+  isConcurrentMode,
+  isContextConsumer,
+  isContextProvider,
+  isElement,
+  isForwardRef,
+  isFragment,
+  isLazy,
+  isMemo,
+  isPortal,
+  isProfiler,
+  isStrictMode,
+  isSuspense,
+  isValidElementType,
+  typeOf
+}
